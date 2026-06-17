f702da90b6a719ce90b67273fac1ba58cb13df37
Smit Gawande <gawandesmit51@gmail.com>
2026-06-17 12:39:42 +0530

    Add files via upload

---
+++ function_basic.py
@@ -0,0 +1,9 @@
+
+#Define a function show_name(name) that takes a string as an argument and prints "Your name is [name]".
+
+def show_name(name):
+    print(f"your name is {name}")
+show_name("smit")
+
+
+            
